98b9b8bc0f55ac94678155d4792ecae88b04e6a9
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-12 22:49:28 +0300

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+        Liza is a Web Developer from New York
+        </h1>
     </div>
   );
 }
+export default Home;
